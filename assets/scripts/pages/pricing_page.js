95f1eb956d00030584472b2d8097211e0700d023
Cao Xuan Thien <caoxuanthien13122002@gmail.com>
2024-03-08 04:16:25 +0700

    update

---
+++ assets/scripts/pages/pricing_page.js
@@ -0,0 +1,34 @@
+import { modeSwitcher } from "../general_functions.js";
+import { getMode } from "../general_functions.js";
+
+modeSwitcher(function () {
+    const currentMode = getMode();
+    if (currentMode === 'light') {
+        $('#navbar').removeClass('navbar-dark bg-dark').addClass('navbar-light bg-light');
+        $('#sign-in-button').removeClass('btn-light').addClass('btn-dark');
+        $('#sign-up-button').removeClass('btn-outline-light').addClass('btn-outline-dark');
+        $('#modeSwitcher').html('<i class="fa fa-moon-o">Dark</i>');
+        $('#logoHeader, #logoFooter').attr('src', './assets/images/logo/rentease-favicon-black.png');
+        $('body').css('background-color', 'white');
+        $('#content').css('color', 'black');
+        $('hr').css('background-color', 'black');
+        $('#free-pricing-card, #pro-pricing-card, #enterprise-pricing-card').css({ "background-color": "rgba(240,240,240)", "color": "black" });
+    } 
+    else {
+        $('#navbar').removeClass('navbar-light bg-light').addClass('navbar-dark bg-dark');
+        $('#sign-in-button').removeClass('btn-dark').addClass('btn-light');
+        $('#sign-up-button').removeClass('btn-outline-dark').addClass('btn-outline-light');
+        $('#modeSwitcher').html('<i class="fa fa-sun-o">Light</i>');
+        $('#logoHeader, #logoFooter').attr('src', './assets/images/logo/rentease-favicon-color.png');
+        $('body').css('background-color', '#383c40');
+        $('#content').css('color', 'white');
+        $('hr').css('background-color', 'white');
+        $('#free-pricing-card, #pro-pricing-card, #enterprise-pricing-card').css({ "background-color": "rgba(56,60,73)", "color": "white" });
+    }
+});
+
+$(document).ready(() => {
+    $('#free-purchase-button').click(() => {
+        document.location.href = 'signup_page.html';
+    });
+});
