fba44612f1df5003a28a44715065455d966791e9
Cao Xuan Thien <caoxuanthien13122002@gmail.com>
2024-03-26 14:15:37 +0700

    update

--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-// echo bin2hex(strrev(base64_encode(']4]t]4]4]tMtM5]t]tMtM4]4Mu]tMu]tMuMtMu]tMt]t]4Mt]4]tM4MtMu]4Mu]4')));
-
-$key = '3d3d516343746d4d6d6c315669563362';
-
-echo base64_decode(strrev(hex2bin($key)));
